d054d0b07e95ed967d210e01b2d0927a7e86a243
Jessie A. Morris <jessie@jessieamorris.com>
2013-12-11 09:35:04 -0700

    Makes the benchmarking code less brittle and more flexible.

--- test/benchmark.js
+++ test/benchmark.js
@@ -1,58 +1,88 @@
 var assert = require("assert");
+var p = require("../promise.js");
 
-var pBench = require('./benchmarks/jpromise.js');
-var qBench = require('./benchmarks/q.js');
+var benchmarks = {
+	"./adapter.js": {},
+	"./benchmarks/qAdapter.js": {}
+};
 
-var promisesAplusTests = require("promises-aplus-tests");
+var jPromiseKey = "./adapter.js";
 
-var pAdapter = require("./adapter.js");
-var qAdapter = require("./benchmarks/qAdapter.js");
 
-var pPass = false;
-var qPass = false;
+var iterations = 10000;
 
-describe("We want to test our speed against Q", function() {
-	it("Should Only happen if We are 100% spec compliant", function(done) {
-		this.timeout(20000);
-		promisesAplusTests(pAdapter, function (err) {
-			if(err === null) {
-				assert(true)
-				pPass = true
-			} else {
-				assert(false, "We didn't pass some tests")
+function benchmark(adapter) {
+	var dummy = {dummy:"dummy"};
+	var resCounter = 1;
+	var rejCounter = 1;
+	var deferred = adapter.deferred();
+	var resolvedDfd = adapter.deferred();
+	var start, stop;
+
+	var ret = {
+		ops: 0,
+		resolvedOps: 0,
+		rejectedOps: 0
+	};
+
+	start = new Date().getTime();
+	for(var i = 0; i < iterations; i++) {
+		var pro = adapter.resolved(dummy).then(function() {
+			resCounter++;
+			if(resCounter === iterations) {
+				stop = new Date().getTime();
+				ret.resolvedOps = iterations / (stop - start) * 1000;
+				resolvedDfd.resolve(true);
 			}
-			done();
 		});
+	}
+
+	resolvedDfd.promise.then(function() {
+		start = new Date().getTime();
+		for(var i = 0; i < iterations; i++) {
+			var pro = adapter.rejected(dummy).then(undefined, function() {
+				rejCounter++;
+				if(rejCounter === iterations) {
+					stop = new Date().getTime();
+
+					ret.rejectedOps = iterations / (stop - start) * 1000;
+
+					// Average out the resolved and rejects to get the total ops/sec
+					ret.ops = (ret.resolvedOps + ret.rejectedOps) / 2;
+					deferred.resolve(ret);
+				}
+			});
+		}
 	});
 
-	it("Should Only happen if Q are 100% spec compliant", function(done) {
-		this.timeout(20000);
-		promisesAplusTests(qAdapter, function (err) {	
-			//BULLSHIT YOU LIAR ... YOU DIE IMMEDIATELY AND CALLING the spec via the CLI you DIE 
-			//AND HANG NOT EVEN A QUARTER OF THE WAY THROUGH
+	return deferred.promise;
+}
 
-			if(err === null) {
-				assert(true)
-				qPass = true
-			} else {
-				assert(false, "Q didn't pass some tests")
-			}
-			done();
+var xhrs = [];
+for(var key in benchmarks) {
+	(function(key) {
+		var adapter = require(key);
+
+		var xhr = benchmark(adapter).then(function(ops) {
+			benchmarks[key].opts = ops;
+		});
+
+		xhrs.push(p.wrap(xhr));
+	})(key);
+}
+
+describe("We want to test our speed against Q", function() {
+	describe("We're faster than everyone else", function() {
+		it("Round 1: indeed, numbers don't lie...", function(done){
+			this.timeout(Object.keys(benchmarks).length * 10000);
+			p.when(xhrs).done(function(){
+				for(var key in benchmarks) {
+					if(key !== jPromiseKey) {
+						assert((benchmarks[jPromiseKey].ops > benchmarks[key].ops), "Dammit, " + key + " was faster");
+					}
+				}
+				done();
+			});
 		});
 	});
 });
-
-if(pPass && qPass) {
-	describe("We're faster than Q", function() {
-		it("Round 1: indeed, numbers don't lie... P.S. ask about how compliant Q is on the promise specs", function(done){
-			this.timeout(10000);
-			pBench().then(function(pData) {
-				qBench().then(function(qData) {
-					assert((pData.ops > qData.ops), "Dammit Q was faster");
-					done();
-				});
-			}, undefined);
-		});
-	});
-}
-
