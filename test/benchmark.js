ed9d3c296519b04cabf06421e0b5b18eaf753045
Jessie A. Morris <jessie@jessieamorris.com>
2013-12-11 10:21:25 -0700

    Actually change benchmarks back. WTF git?

--- test/benchmark.js
+++ test/benchmark.js
@@ -1,21 +1,17 @@
 var assert = require("assert");
-var p = require("../promise.js");
 
-var benchmarks = {
-	"./adapter.js": {},
-	"./benchmarks/qAdapter.js": {}
-};
+var pBench = require('./benchmarks/jpromise.js');
+var qBench = require('./benchmarks/q.js');
 
-describe("We want to test our speed against Q", function() {
-	describe("We're faster than Q", function() {
-		it("Round 1: indeed, numbers don't lie... P.S. ask about how compliant Q is on the promise specs", function(done){
-			this.timeout(10000);
-			pBench().then(function(pData) {
-				qBench().then(function(qData) {
-					assert((pData.ops > qData.ops), "Dammit Q was faster");
-					done();
-				});
-			}, undefined);
-		});
+describe("We're faster than Q", function() {
+	it("Round 1: indeed, numbers don't lie... P.S. ask about how compliant Q is on the promise specs", function(done){
+		this.timeout(10000);
+		pBench().then(function(pData) {
+			qBench().then(function(qData) {
+				assert((pData.ops > qData.ops), "Dammit Q was faster");
+				done();
+			});
+		}, undefined);
 	});
 });
+
